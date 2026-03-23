f8472d69d63ded796100d3a3488f94659a5db428
shivam24s <shivamshrivastav8055@gmail.com>
2026-03-23 10:05:14 +0530

    o-auth completed

--- 11_O-auth/config/passport.js
+++ 11_O-auth/config/passport.js
@@ -31,4 +31,18 @@ passport.use(
   ),
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
 export default passport;
