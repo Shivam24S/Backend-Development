28841a50cd25907eb009c716133406f644cb908a
shivam24s <shivamshrivastav8055@gmail.com>
2026-03-25 10:14:59 +0530

    user adding and login

--- 12_quickNest/server.js
+++ 12_quickNest/server.js
@@ -1,15 +1,33 @@
 import dotenv from "dotenv";
-
 dotenv.config({ path: "./.env" });
 
 import express from "express";
+
+import HttpError from "./middleware/HttpError.js";
+import connectDB from "./config/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/user", userRouter);
+
 app.get("/", (req, res) => {
   res.json("hello from server");
+});
+
+app.use((req, res, next) => {
+  return next(new HttpError("requested route not found", 404));
+});
+
+app.use((error, req, res, next) => {
+  if (res.headersSent) {
+    next(error);
+  }
+  res
+    .status(error.statusCode || 500)
+    .json(error.message || "internal server error");
 });
 
 const port = process.env.PORT || 5000;
@@ -18,5 +36,15 @@ console.log("port", port);
 
 async function startServer() {
   try {
-  } catch (error) {}
+    await connectDB();
+
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
+
+startServer();
